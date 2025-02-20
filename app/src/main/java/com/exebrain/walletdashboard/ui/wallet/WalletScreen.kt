1463d7236328bab517b6ab9f8504bccf7ba52cc9
jason.j <s124202468@gmail.com>
2025-02-20 19:29:09 +0800

    Refine wallet screen and viewmodel.

--- app/src/main/java/com/exebrain/walletdashboard/ui/wallet/WalletScreen.kt
+++ app/src/main/java/com/exebrain/walletdashboard/ui/wallet/WalletScreen.kt
@@ -1,25 +1,18 @@
 package com.exebrain.walletdashboard.ui.wallet
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.exebrain.walletdashboard.ui.components.ErrorState
 
@@ -29,7 +22,7 @@ fun WalletScreen(
     viewModel: WalletViewModel = viewModel(),
     innerPadding: PaddingValues
 ) {
-    val walletUiState by viewModel.walletUiState.collectAsState()
+    val walletUiState by viewModel.walletUiState.collectAsStateWithLifecycle()
 
     Column(
         modifier = Modifier
@@ -39,20 +32,18 @@ fun WalletScreen(
     ) {
         BalanceDisplay(walletUiState.balance)
 
-        if (walletUiState.errorMessage.isNullOrBlank()) {
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-                    .padding(top = 8.dp),
-                color = Color.White
-            ) {
-                CryptoAssetList(assets = walletUiState.assets)
-            }
-        } else {
+        walletUiState.errorMessage?.takeIf { it.isNotBlank() }?.let { errMsg->
             ErrorState(
-                errorMessage = walletUiState.errorMessage!!,
+                errorMessage = errMsg,
                 onRetryClick = { viewModel.onRetry() })
+        }?: Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(top = 8.dp),
+            color = Color.White
+        ) {
+            CryptoAssetList(assets = walletUiState.assets)
         }
     }
 }
