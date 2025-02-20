abd47959be6d38b907cfe8a5e19eef9abe452836
jason.j <s124202468@gmail.com>
2025-02-20 13:48:24 +0800

    Add error state for ui.

--- app/src/main/java/com/exebrain/walletdashboard/ui/wallet/WalletScreen.kt
+++ app/src/main/java/com/exebrain/walletdashboard/ui/wallet/WalletScreen.kt
@@ -1,19 +1,27 @@
 package com.exebrain.walletdashboard.ui.wallet
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.exebrain.walletdashboard.ui.components.ErrorState
 
 
 @Composable
@@ -30,14 +38,22 @@ fun WalletScreen(
             .background(Color(0xFF003459))
     ) {
         BalanceDisplay(walletUiState.balance)
-        Surface(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .padding(top = 8.dp),
-            color = Color.White
-        ) {
-            CryptoAssetList(assets = walletUiState.assets)
+
+        if (walletUiState.errorMessage.isNullOrBlank()) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .padding(top = 8.dp),
+                color = Color.White
+            ) {
+                CryptoAssetList(assets = walletUiState.assets)
+            }
+        } else {
+            ErrorState(
+                errorMessage = walletUiState.errorMessage!!,
+                onRetryClick = { viewModel.onRetry() })
         }
     }
 }
+
